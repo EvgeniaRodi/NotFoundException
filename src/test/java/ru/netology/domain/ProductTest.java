8571468687b467ecc46604a220aab251b614af94
Rodionova Evgenia <jenechkajj353@gmail.com>
2021-06-06 15:14:22 +0300

    newproject

---
+++ src/test/java/ru/netology/domain/ProductTest.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+class ProductTest {
+//  @Test
+//  public void shouldUseEquals() {
+//    Product first = new Product(1, "Java I", 1000);
+//    Product second = new Product(1, "Java I", 1000);
+//    assertEquals(first, second);
+//  }
+}
